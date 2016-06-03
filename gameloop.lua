8530812cf39c9860dee7af66f18fb783b4450d05
Stew Esho <StewardEsho@gmail.com>
2016-06-03 18:59:30 -0400

    Added Basic Enemy class

--- gameloop.lua
+++ gameloop.lua
@@ -1,16 +1,19 @@
----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 --
 -- gameloop.lua
 --
----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+local baseEnemy = require("enemy");
 local gameloop = {}
 
+local enemy1;
+
 function gameloop:init()
-  
+  enemy1 = baseEnemy:new(display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
 end
 
 function gameloop:run()
-
+  enemy1:run();
 end
 
 return gameloop;
