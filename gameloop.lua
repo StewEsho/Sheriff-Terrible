baa591e6cf54404b7abeb2767671b62135e6caf0
Stew Esho <StewardEsho@gmail.com>
2016-06-03 19:26:23 -0400

    Made the screen sideways once again

--- gameloop.lua
+++ gameloop.lua
@@ -12,7 +12,7 @@ local movementX, movementY = 0
 local enemy1;
 
 function gameloop:init()
-    AudioManager:playSound("sounds/funnymusic.mp3")
+  --AudioManager:playSound("sounds/funnymusic.mp3")
   enemy1 = baseEnemy:new(display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
 end
 
