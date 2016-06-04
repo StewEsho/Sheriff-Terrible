f9fde957617b9cbbd8d6838d906cb6e34a35c94f
Stew Esho <StewardEsho@gmail.com>
2016-06-03 20:19:23 -0400

    Added Dolphin enemy

--- gameloop.lua
+++ gameloop.lua
@@ -15,13 +15,13 @@ function gameloop:init()
   --AudioManager:playSound("sounds/funnymusic.mp3")
   enemy1 = baseEnemy:new(display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
   enemy:init();
-  enemy:spawn(1, display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
-  enemy:spawn(1, 300, 25, {sprite = "img/sprite/placeholderEnemy1.png"});
-  enemy:spawn(1, 400, 50, {sprite = "img/sprite/placeholderEnemy1.png"});
-  enemy:spawn(1, 200, 75, {sprite = "img/sprite/placeholderEnemy1.png"});
-  enemy:spawn(1, 100, 200, {sprite = "img/sprite/placeholderEnemy1.png"});
-  enemy:spawn(1, 50, 300, {sprite = "img/sprite/placeholderEnemy1.png"});
-  enemy:spawn(1, 700, 400, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, display.contentWidth/2, display.contentHeight/2)
+  enemy:spawn(1, 300, 25);
+  enemy:spawn(1, 400, 50);
+  enemy:spawn(2, 200, 75);
+  enemy:spawn(2, 100, 200);
+  enemy:spawn(2, 50, 300);
+  enemy:spawn(2, 700, 400);
 end
 
 function gameloop:run()
