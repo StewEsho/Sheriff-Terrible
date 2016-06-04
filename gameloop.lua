ee8317f115c23e858a4bd5eb6b6943fab16a819f
Stew Esho <StewardEsho@gmail.com>
2016-06-03 20:07:41 -0400

    Made cowboy enemy modular

--- gameloop.lua
+++ gameloop.lua
@@ -3,17 +3,24 @@
 -- gameloop.lua
 --
 --------------------------------------------------------------------------------
-local baseEnemy = require("enemy");
+local enemy = require("enemymanager");
 local gameloop = {}
 
 local enemy1;
 
 function gameloop:init()
-  enemy1 = baseEnemy:new(display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
+  enemy:init();
+  enemy:spawn(1, display.contentWidth/2, display.contentHeight/2, {sprite = "img/sprite/placeholderEnemy1.png"})
+  enemy:spawn(1, 300, 25, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, 400, 50, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, 200, 75, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, 100, 200, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, 50, 300, {sprite = "img/sprite/placeholderEnemy1.png"});
+  enemy:spawn(1, 700, 400, {sprite = "img/sprite/placeholderEnemy1.png"});
 end
 
 function gameloop:run()
-  enemy1:run();
+  enemy:run();
 end
 
 return gameloop;
