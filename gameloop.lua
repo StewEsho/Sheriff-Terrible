ac37b175bdd4cb210507892ae4ebd488c2e0e2d8
Stew Esho <StewardEsho@gmail.com>
2016-06-24 13:28:39 -0400

    Added background "scenery"

--- gameloop.lua
+++ gameloop.lua
@@ -13,10 +13,16 @@ local timer;
 local runtime;
 local spawnTime;
 local title, subtitle;
+local skyBG;
 
 function gameloop:init()
   --AudioManager:playSound("music/action1.mp3", 1, -1);
   AudioManager:setMusicVolume(1);
+
+  display.setDefault("background", 0.8, 0.5, 0.25)
+  skyBG = display.newRect(display.actualContentWidth/2, 0, display.actualContentWidth, 150);
+  skyBG:setFillColor(0.2, 0.6, 0.8);
+  skyBG.path.y3 = skyBG.path.y3 - 16
 
   title = display.newText("Sherrif-Terrible", display.contentWidth/2, 12, "font/CabinSketch-Bold.ttf", 42);
   title:setFillColor(1, 0.7, 0.1);
@@ -33,7 +39,7 @@ end
 function gameloop:run()
   timer = timer + getDeltaTime();
   if (timer > spawnTime and #enemy:get(1) < 3) then
-    enemy:spawn(1, math.random(0, display.contentWidth), math.random(60, display.contentHeight-60));
+    enemy:spawn(1, math.random(0, display.contentWidth), math.random(150, display.contentHeight-60));
     timer = 0;
     spawnTime = math.random(45, 105);
   end
